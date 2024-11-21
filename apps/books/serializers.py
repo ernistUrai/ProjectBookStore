a4d06ea80aa503bd3d2283a3179729033c107c04
Ernst <ernistturgunbaev01@gmail.com>
2024-11-21 18:36:59 +0500

    update

--- apps/books/serializers.py
+++ apps/books/serializers.py
@@ -21,7 +21,7 @@ class CategorySerializer(serializers.ModelSerializer):
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = '__all__'        # fields = ['id', 'title', 'author', 'price', 'created_at', 'updated_at']
+        fields = '__all__'       
 
 
 class OrderSerializer(serializers.ModelSerializer): 
