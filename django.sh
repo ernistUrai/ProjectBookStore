1c728771b520f5b0dfb121a05534c54453f7316d
Ernst <ernistturgunbaev01@gmail.com>
2025-01-12 18:58:16 +0500

    Update

--- django.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Django командасын иштетүү
-./manage.py collectstatic --noinput
-./manage.py makemigrations
-./manage.py migrate
-gunicorn -b 0.0.0.0:8000 core.wsgi:application
--- main/settings.py
+++ main/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 APPEND_SLASH = False
 
@@ -86,11 +86,8 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'db'), 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),   
     }
 }
 
